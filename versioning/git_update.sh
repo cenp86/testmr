4d9ae18f960c7c25ec2aaf911b877278f202ab5f
Carlos Narvaez <cenp86@gmail.com>
2025-04-03 11:41:45 -0500

    workflow updates

--- versioning/git_update.sh
+++ versioning/git_update.sh
@@ -11,7 +11,7 @@ do
 done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
-CURRENT_VERSION=$(git for-each-ref --sort=-committerdate --format '%(refname:strip=2)' refs/tags | head -n 1)
+CURRENT_VERSION=$(git for-each-ref --sort=-committerdate --format '%(refname:strip=2)' refs/tags)
 
 if [[ $CURRENT_VERSION == '' ]]
 then
